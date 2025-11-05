52f47de3429486e7b1d20c487e6b92b58fbddcf7
Christian <MbolanantenainaChristian@gmail.com>
2025-11-05 15:19:22 +0300

    Last modif

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmbolana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 15:11:45 by jmbolana          #+#    #+#             */
+/*   Updated: 2025/11/05 15:19:00 by jmbolana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_sort_int_tab(int *tab, int size);
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	j;
+	int	temp;
+
+	while (size > 0)
+	{
+		j = 0;
+		while (j < size)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+			}
+			j++;
+		}
+		size--;
+	}
+}
